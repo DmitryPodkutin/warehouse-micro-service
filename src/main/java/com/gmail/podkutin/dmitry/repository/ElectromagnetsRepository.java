2690a136682af560c8274b9d1d309406c9a4312c
Dmitry_Podkutin <dmitry.podkutin@gmail.com>
2022-12-06 16:44:38 +0600

    Create getByModel Query

--- src/main/java/com/gmail/podkutin/dmitry/repository/ElectromagnetsRepository.java
+++ src/main/java/com/gmail/podkutin/dmitry/repository/ElectromagnetsRepository.java
@@ -1,12 +1,15 @@
 package com.gmail.podkutin.dmitry.repository;
 
 import com.gmail.podkutin.dmitry.model.Electromagnet;
+import com.gmail.podkutin.dmitry.model.HydraulicValve;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ElectromagnetsRepository extends CommonRepository<Electromagnet> {
@@ -15,4 +18,7 @@ public interface ElectromagnetsRepository extends CommonRepository<Electromagnet
     @Modifying(clearAutomatically = true)
     @Query("DELETE FROM Electromagnet e where e.id =:id")
     int delete(@Param("id") Integer id);
+
+    @Query("SELECT e FROM Electromagnet e where e.electromagnetModel.model =:model")
+    Optional<List<Electromagnet>> getByModel(@Param("model") String model);
 }
--- src/main/java/com/gmail/podkutin/dmitry/repository/HydraulicValvesRepository.java
+++ src/main/java/com/gmail/podkutin/dmitry/repository/HydraulicValvesRepository.java
@@ -1,19 +1,22 @@
 package com.gmail.podkutin.dmitry.repository;
 
 import com.gmail.podkutin.dmitry.model.HydraulicValve;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Repository
-public interface HydraulicValvesRepository extends  CommonRepository<HydraulicValve> {
+public interface HydraulicValvesRepository extends CommonRepository<HydraulicValve> {
 
     @Transactional
     @Modifying(clearAutomatically = true)
     @Query("DELETE FROM HydraulicValve hv where hv.id =:id")
     int delete(@Param("id") Integer id);
+
+    @Query("SELECT hv FROM HydraulicValve hv where hv.model =:model")
+    Optional<HydraulicValve> getByModel(@Param("model") String model);
 }
