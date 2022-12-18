e25f4e41523179319846d0acb136e6b1ae9acf43
Dmitry_Podkutin <dmitry.podkutin@gmail.com>
2022-12-18 11:04:59 +0300

    Label updated with new method -  getByModel

--- src/main/java/com/gmail/podkutin/dmitry/repository/LabelsRepository.java
+++ src/main/java/com/gmail/podkutin/dmitry/repository/LabelsRepository.java
@@ -1,13 +1,13 @@
 package com.gmail.podkutin.dmitry.repository;
 
 import com.gmail.podkutin.dmitry.model.Label;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Repository
 public interface LabelsRepository  extends  CommonRepository<Label>{
@@ -16,4 +16,7 @@ public interface LabelsRepository  extends  CommonRepository<Label>{
     @Modifying(clearAutomatically = true)
     @Query("DELETE FROM Label lb where lb.id =:id")
     int delete(@Param("id") int id);
+
+    @Query("SELECT lb FROM Label lb where lb.model =:model")
+    Optional<Label> getByModel(@Param("model") String model);
 }
--- src/main/java/com/gmail/podkutin/dmitry/service/impl/LabelServiceImpl.java
+++ src/main/java/com/gmail/podkutin/dmitry/service/impl/LabelServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gmail.podkutin.dmitry.service.impl;
 
+import com.gmail.podkutin.dmitry.exeption.NotFoundException;
+import com.gmail.podkutin.dmitry.model.HydraulicValve;
 import com.gmail.podkutin.dmitry.model.Label;
 import com.gmail.podkutin.dmitry.repository.LabelsRepository;
 import com.gmail.podkutin.dmitry.service.AbstractService;
@@ -9,7 +11,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class LabelServiceImpl extends AbstractService<Label, LabelsRepository> {
 
+    private final LabelsRepository repository;
+
     public LabelServiceImpl(@Autowired LabelsRepository repository) {
         super(repository);
+        this.repository = repository;
+    }
+
+    public Label getByModel(String model) {
+        return repository.getByModel(model).orElseThrow(() ->
+                new NotFoundException("Label model +" + model + " Not Found"));
     }
 }
