205fc86b70105548e70801f3d0422eb62797fc19
Dmitry_Podkutin <dmitry.podkutin@gmail.com>
2022-12-26 16:13:02 +0300

    Added new methods to KitOfHydraulicValveController

--- src/main/java/com/gmail/podkutin/dmitry/contorller/KitOfHydraulicValveController.java
+++ src/main/java/com/gmail/podkutin/dmitry/contorller/KitOfHydraulicValveController.java
@@ -1,7 +1,5 @@
 package com.gmail.podkutin.dmitry.contorller;
 
-import com.gmail.podkutin.dmitry.exeption.EquipmentException;
-import com.gmail.podkutin.dmitry.exeption.NotFoundException;
 import com.gmail.podkutin.dmitry.model.KitOfHydraulicValve;
 import com.gmail.podkutin.dmitry.model.dto.KitOfHydraulicValveDTO;
 import com.gmail.podkutin.dmitry.service.KitService;
@@ -12,42 +10,44 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/warehouse/kits-of-hydraulic-valves")
 public class KitOfHydraulicValveController {
 
-    private final KitService<KitOfHydraulicValveDTO,KitOfHydraulicValve> kitService;
+    private final KitService<KitOfHydraulicValveDTO, KitOfHydraulicValve> kitService;
 
     public KitOfHydraulicValveController(@Autowired KitOfHydraulicValveServiceImpl kitService) {
         this.kitService = kitService;
     }
 
     @PreAuthorize("hasAuthority('write')")
+    @PostMapping()
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void save(@RequestBody List<KitOfHydraulicValveDTO> kits) {
+        kitService.save(kits);
+    }
+
+    @PreAuthorize("hasAuthority('write')")
     @DeleteMapping()
-    ResponseEntity<KitOfHydraulicValve> sale(@RequestBody KitOfHydraulicValveDTO entity) {
-        KitOfHydraulicValve kit = kitService.sale(entity);
-        return kit != null
-                ? new ResponseEntity<>(kit, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.CONFLICT);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void sale(@RequestBody KitOfHydraulicValveDTO kit) {
+        kitService.sale(kit);
     }
 
     @PreAuthorize("hasAuthority('read')")
     @GetMapping()
-    ResponseEntity<KitOfHydraulicValve> get(@RequestBody KitOfHydraulicValveDTO entity) {
-        KitOfHydraulicValve kit = kitService.get(entity);
-        return kit != null
-                ? new ResponseEntity<>(kit, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @PreAuthorize("hasAuthority('read')")
-    @GetMapping("/filter")
-    ResponseEntity<List<KitOfHydraulicValve>> getAllFiltered(@RequestBody List<KitOfHydraulicValveDTO> entity) {
-        List<KitOfHydraulicValve> kits = kitService.getAllFiltered(entity);
-        return kits != null
-                ? new ResponseEntity<>(kits, HttpStatus.OK)
+    ResponseEntity<List<KitOfHydraulicValve>> getKits(@RequestBody List<KitOfHydraulicValveDTO> kits) {
+        List<KitOfHydraulicValve> result = new ArrayList<>();
+        if (kits.size() == 1) {
+            result.add(kitService.get(kits.get(0)));
+        } else {
+            result = kitService.getList(kits);
+        }
+        return result != null
+                ? new ResponseEntity<>(result, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
